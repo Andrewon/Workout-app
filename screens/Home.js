202bf9e2456cb0e72ac491fc09810ec9d80c60f2
Andrewon <duc.tm8@gmail.com>
2021-07-27 13:32:29 -0500

    header welcome

--- screens/Home.js
+++ screens/Home.js
@@ -8,15 +8,35 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
+import CategoryCard from "../components/CategoryCard";
 
 import { FONTS, COLORS, SIZES, images, icons, dummyData } from "../constants";
+
+function renderHeader() {
+  <View
+    style={{
+      flexDirection: "row",
+      marginHorizontal: SIZES.padding,
+      alignItems: "center",
+      height: 80,
+    }}
+  >
+    {/* Hello text */}
+    <View style={{ flex: 1 }}>
+      <Text style={{ color: COLORS.darkGreen, ...FONTS.h2 }}>Hello Andy,</Text>
+      <Text style={{ marginTop: 3, color: COLORS.gray, ...FONTS.body3 }}>
+        What are you working out today?
+      </Text>
+    </View>
+  </View>;
+}
 
 function ParseData() {
   const [Datas, setDatas] = React.useState(null);
 
   React.useEffect(() => {
     dummyData.db.transaction((tx) => {
-      tx.executeSql("select * from recipes", [], (_, { rows: { _array } }) =>
+      tx.executeSql("select * from recipes4", [], (_, { rows: { _array } }) =>
         setDatas(_array)
       );
     });
@@ -30,15 +50,27 @@ function ParseData() {
       }}
     >
       <FlatList
-        data={Datas}
+        data={dummyData.categories}
         keyExtractor={(item) => "${item.id}"}
         keyboardDismissMode="on-drag"
         showsVerticalScrollIndicator={false}
-        ListHeaderComponent={<View></View>}
+        ListHeaderComponent={
+          <View>
+            {/* Header */}
+            {renderHeader()}
+            {/* Search bar */}
+            {/* See workout card */}
+            {/* category Header */}
+          </View>
+        }
         renderItem={({ item }) => {
           return (
             <View>
-              <Text>{item.name}</Text>
+              <CategoryCard
+                containerStyle={{ marginHorizontal: SIZES.padding }}
+                categoryItem={item}
+                onPress={() => navigation.navigate("Recipe", { recipe: item })}
+              />
             </View>
           );
         }}
@@ -61,28 +93,20 @@ function DataInit() {
       execute = true;
       dummyData.db.transaction((tx) => {
         tx.executeSql(
-          "create table if not exists recipes (id integer primary key not null, name string, duration string, serving integer, category string );"
+          "create table if not exists recipes4 (id integer primary key not null autoincrement, name string, duration string, serving integer, category string );"
         );
-        console.log("database created");
-        tx.executeSql("select * from recipes", [], (_, { rows }) =>
-          console.log(JSON.stringify(rows) + "hi")
+        //prettier-ignore
+        tx.executeSql("insert into recipes4 (id,name,duration,serving,category)",[1, "andy", "30 mins", 1, "Local"]);
+        tx.executeSql("select * from recipes4", [], (_, { rows }) =>
+          console.log(JSON.stringify(rows) + "recipes")
         );
       });
     }
-    dummyData.db.transaction((tx) => {
-      tx.executeSql();
-      tx.executeSql();
-
-      tx.executeSql("select * from recipes", [], (_, { rows }) =>
-        console.log(JSON.stringify(rows) + "hi")
-      );
-    });
   }, []);
 }
 
 var execute = false;
 const Home = ({ navigation }) => {
-  DataInit();
   return <ParseData></ParseData>;
 };
 
