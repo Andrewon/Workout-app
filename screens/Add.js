1b13dc4c1ea3f0d4069ffcf04c5a3f1a1dc23a8f
Andrewon <duc.tm8@gmail.com>
2021-07-28 11:18:05 -0500

    add screen init

---
+++ screens/Add.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { View } from "react-native";
+
+const Add = () => {
+  return (
+    <View>
+      <Text>Add screen</Text>
+    </View>
+  );
+};
+
+export default Add;
--- screens/index.js
+++ screens/index.js
@@ -1,9 +1,6 @@
-import Login from "./Login"
-import Home from "./Home"
-import Recipe from "./Recipe"
+import Login from "./Login";
+import Home from "./Home";
+import Recipe from "./Recipe";
+import Add from "./Add";
 
-export {
-    Login,
-    Home,
-    Recipe
-}
+export { Login, Home, Recipe, Add };
